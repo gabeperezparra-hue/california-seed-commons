fd570f80f7a144c6b6e98de65b011671a77947a0
gabeperezparra-hue <gabeperezparra@gmail.com>
2026-05-09 15:17:06 -0700

    Create page.tsx

---
+++ app/admin/requests/page.tsx
@@ -0,0 +1,10 @@
+export default function RequestsPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-medium mb-6">Submitted Requests</h1>
+      <div className="border p-8 text-center text-stone-500">
+        List of all requests will appear here.
+      </div>
+    </div>
+  );
+}
