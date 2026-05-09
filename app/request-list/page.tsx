0c473abd639116fc026e77d0a092ef4d6f5320d4
gabeperezparra-hue <gabeperezparra@gmail.com>
2026-05-09 15:11:01 -0700

    Create page.tsx

---
+++ app/request-list/page.tsx
@@ -0,0 +1,8 @@
+export default function RequestListPage() {
+  return (
+    <div className="max-w-4xl mx-auto px-6 py-12">
+      <h1 className="text-4xl font-medium mb-8">Your Request List</h1>
+      <p className="text-stone-600">No items yet. Go to Browse Seeds to add some.</p>
+    </div>
+  );
+}
